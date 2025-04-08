46ebe16ac77b66541517599d896a39878789a00b
AngusN08 <angus.wc.nash@icloud.com>
2025-04-08 02:57:40 +0000

    completed warmups

--- main.py
+++ main.py
@@ -1,16 +1,25 @@
 
 # Input a number from the user and print the square of the number in a message
-
+# number = float(input('Enter a number: '))
+# square_num = number ** 2
+# print(square_num)
 
 
 
 # input a word and blank out all letters that are not vowels with '='
-
+# word = input('Enter a word: ').lower()
+# for letter in word:
+#     if letter == 'a' or 'e' or 'i' or 'o' or 'u':
+#         new_word = word.replace('a', '=').replace('e', '=').replace('i', '=').replace('o', '=').replace('u', '=')
+# print(new_word)
 
 
 # Create a list of words - go through the list and sum the total number of characters in all words
-
-
+filled_list = ['hello', 'how', 'are', 'you', 'today']
+for word in filled_list:
+    for letter in word:
+        
+print(count)
 
 # Creata a list of words - count the total number of DIFFERENT characters in all the words
 
